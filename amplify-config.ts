14041df0243f8cff77a42ca781e11e478dfc7021
Baff Boakye <baff@MacBookPro.attlocal.net>
2025-04-21 10:19:28 -0500

    updates

--- amplify-config.ts
+++ amplify-config.ts
@@ -5,9 +5,9 @@ export const SAML_METADATA_URL =
   "https://login.microsoftonline.com/c0afaa63-ec73-48c9-bc2c-49e1f1d9c63e/federationmetadata/2007-06/federationmetadata.xml?appid=1ae80849-7ca9-4bbb-9030-eaa3cbd445f2";
 
 export const CALLBACK_URLS = [
-  "https://default-cognito.d14p5z06ominpn.amplifyapp.com"
+  "https://logarchive-auth.d35s4b1b3cpy1s.amplifyapp.com"
 ];
 
 export const LOGOUT_URLS = [
-  "https://default-cognito.d14p5z06ominpn.amplifyapp.com"
+  "https://logarchive-auth.d35s4b1b3cpy1s.amplifyapp.com"
 ];
